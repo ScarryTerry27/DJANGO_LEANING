63830d664a5a586cf0a519c3e359518a8b1cdc9d
Ilyas Khasakhanov <clauster27@gmail.com>
2023-09-04 13:32:49 +0400

    create_admin_desktop

--- scaryterry/women/models.py
+++ scaryterry/women/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Имя')
+    content = models.TextField(blank=True, verbose_name='Информация')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -17,14 +17,24 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Известные супергеройки'
+        verbose_name_plural = 'Известные супергеройки'
+        ordering = ['-time_create', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
 
 
